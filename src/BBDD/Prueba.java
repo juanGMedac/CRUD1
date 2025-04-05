8b5c3529fe08e97e00176acb3a7998198e9fdedf
JuanGMedac <juan.garcia@doc.medac.es>
2025-04-05 02:52:03 +0200

    Initial commit

---
+++ src/BBDD/Prueba.java
@@ -0,0 +1,36 @@
+package BBDD;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Prueba {
+
+    public static void main(String[] args){
+
+
+        String sentencia = "INSERT INTO Personas(Nombre, Edad) VALUES ('Pablo', 18)";
+        String busqueda = "SELECT * FROM Personas";
+        ConexionMySQL conexion = new ConexionMySQL("usuario", "pass", "bd");
+
+        try {
+            conexion.conectar();
+            conexion.ejecutarInsertDeleteUpdate(sentencia);
+
+            ResultSet resultado = conexion.ejecutarSelect(busqueda);
+
+            while (resultado.next()) {
+                String nombre = resultado.getString("Nombre");
+                int edad = resultado.getInt("Edad");
+                System.out.println("Nombre: " + nombre + ", Edad: " + edad);
+            }
+
+            resultado.close();
+            conexion.desconectar();
+
+        } catch (SQLException e) {
+            System.err.println("Error en la base de datos: " + e.getMessage());
+        }
+
+    }
+
+}
